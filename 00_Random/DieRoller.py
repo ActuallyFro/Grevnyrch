bb78496c6c35161f1d445713577f9180e0c7b223
actuallyfro <actuallyfro@github.com>
2022-08-28 18:51:57 -0500

    {00_Random} v1.0.0 of the DieRoller done

--- 00_Random/DieRoller.py
+++ 00_Random/DieRoller.py
@@ -1,6 +1,14 @@
+from itertools import count
 import random
 import os
     
+def clear():
+   if os.name == 'nt':
+      _ = os.system('cls')
+
+   else:
+    _ = os.system('clear')
+
 def PrintDieSelection():
     print("\nSelect a die to roll:")
     print("1. d4")
@@ -9,11 +17,22 @@ def PrintDieSelection():
     print("4. d10")
     print("5. d12")
     print("6. d20")
+    print("\n")
+    # q for quit
+    print("q. Quit")
+
     return
 
 def AskForDiePick():
-    die_selection = int(input("\nEnter your selection: "))
+    die_selection = 0
+    userInput =input("\nEnter your selection: ")
+
+    if userInput.isdigit():
+        die_selection = int(userInput)
     
+    if userInput == "q":
+        return -99
+
     if die_selection == 1:
         return 4
     elif die_selection == 2:
@@ -32,25 +51,30 @@ def AskForDiePick():
     return -1
 
 def main():
-    os.system('cls')
-
+    
     die_picked = False
     isUserDone = False
     upper_limit = 0
+    counter = 0
 
     while not isUserDone:
         if not die_picked:
+            clear()
             PrintDieSelection()
             upper_limit = AskForDiePick()
             if upper_limit <= 0:
+                if upper_limit == -99:
+                    isUserDone = True
                 continue
             die_picked = True
+            counter = 0
 
         else:
             die_roll = random.randint(1, upper_limit)
-            print("\nYou rolled a", die_roll)
+            counter += 1
+            print("\n[", counter ,"] You rolled a ⦗", die_roll, "⦘")
 
-            user_input = input("\nRoll again? (y/n): ")
+            user_input = input("\n\tRoll again? (y/n): ")
 
             if user_input == "n":
                 isUserDone = True
