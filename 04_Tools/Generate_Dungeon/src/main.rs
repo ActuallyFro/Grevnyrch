c5b1ba795e939558eb16550547dafd1cb1d2bd5a
actuallyfro <actuallyfro@github.com>
2023-01-29 09:22:10 -0600

    {Generate_Dungeon} World Selection done...

--- 04_Tools/Generate_Dungeon/src/main.rs
+++ 04_Tools/Generate_Dungeon/src/main.rs
@@ -47,6 +47,8 @@ struct WorldDungeon {
   selected_world: i32,
   max_worlds: i32,
   existing_worlds: i32,
+  world_words: i32,
+  name : String,
 }
 
 // Grevnyrch Dungeon Struct
@@ -88,6 +90,8 @@ fn main() {
     selected_world: 0,
     max_worlds: 16,
     existing_worlds: 0,
+    world_words: 2,
+    name: String::new(),
   };
 
   // Grevnyrch Dungeon
@@ -143,14 +147,26 @@ fn main() {
           println!("[DEBUG] arg: {}, next_arg: {}", arg, next_arg);
         }
 
-        if arg == "--name" {
+        if arg == "--name-dungeon" {
           dungeon.name = next_arg.to_string();
           println!("[SETTING] world name: {}", dungeon.name);
+
+        } else if arg == "--name-world" {
+          multiverse_world_dungeon.name = next_arg.to_string();
+          println!("[SETTING] world name: {}", multiverse_world_dungeon.name);
 
         } else if arg == "--existing-worlds" {
           multiverse_world_dungeon.existing_worlds = next_arg.trim().parse().unwrap();
 
           println!("[SETTING] existing_worlds: {}", multiverse_world_dungeon.existing_worlds);
+
+        } else if arg == "--world-words" {
+          multiverse_world_dungeon.world_words = next_arg.trim().parse().unwrap();
+
+          println!("[SETTING] world_words: {}", multiverse_world_dungeon.world_words);
+
+        } else {
+          println!("[WARNING] Skipping unknown flag: {}", arg);
         }
         args_index += 1;
         skip_read = true;
@@ -165,8 +181,8 @@ fn main() {
     multiverse_world_dungeon.is_world_in_multiverse = true;
   }
 
-  if verbose {
-    println!("[VERBOSE] 1. is_world_in_multiverse: {} (rolled: {})", multiverse_world_dungeon.is_world_in_multiverse, rng_multiverse);
+  if debug {
+    println!("[DEBUG] is_world_in_multiverse: {} (rolled: {})", multiverse_world_dungeon.is_world_in_multiverse, rng_multiverse);
   }
 
   if multiverse_world_dungeon.is_world_in_multiverse {
@@ -174,8 +190,40 @@ fn main() {
     //Pick a world from the multiverse
     let rng_multiverse_existing: i32 = random_number(1, 100);
     if rng_multiverse_existing >= 67 {
+      println!("[WORLD] == NEW Multiverse World ===");
       multiverse_world_dungeon.selected_world = random_number(1, multiverse_world_dungeon.max_worlds);
+
+      if multiverse_world_dungeon.name == "" {
+        println!("[NOTICE] Go to webpage: https://www.fantasynamegenerators.com/world-names.php");
+        //create vector to block repeat words
+        let mut used_words: Vec<i32> = Vec::new();
+  
+        while used_words.len() < multiverse_world_dungeon.world_words as usize {
+          let rng_world_word: i32 = random_number(1, 20);
+          
+          //check if rng_world_word is in used_words
+          if !used_words.contains(&rng_world_word) {
+            used_words.push(rng_world_word);
+
+            if debug {
+              println!("[DEBUG] For World Name: Use word: {}", rng_world_word);
+            }
+          }
+        }
+
+        // convert vector to string with values inside <>
+        for word in used_words.iter() {
+          multiverse_world_dungeon.name.push_str("<");
+          multiverse_world_dungeon.name.push_str(&word.to_string());
+          multiverse_world_dungeon.name.push_str("> ");
+        }
+        //remove trailing space
+        multiverse_world_dungeon.name.pop();
+      }
+
     } else {
+      println!("[WORLD] === Existing Multiverse World === ");
+
       if multiverse_world_dungeon.existing_worlds == 0 {
         println!("[Input] Enter the max range of number of existing worlds: ");
         io::stdout().flush().unwrap();
@@ -183,9 +231,17 @@ fn main() {
         io::stdin().read_line(&mut existing_worlds_str).unwrap();
         multiverse_world_dungeon.existing_worlds = existing_worlds_str.trim().parse().unwrap();
       }
+
       multiverse_world_dungeon.selected_world = random_number(1, multiverse_world_dungeon.existing_worlds);
+      multiverse_world_dungeon.name = format!("<Look Up Name!>");
+
     }
+
+    println!("[WORLD] Selected World: {}", multiverse_world_dungeon.selected_world);
+    println!("[WORLD] World Name: {}", multiverse_world_dungeon.name);
   
+  } else {
+    println!("[WORLD] === Grevnyrch ===");
   }
 
 
@@ -198,7 +254,14 @@ fn main() {
   }
 
   if verbose {
-    println!("[VERBOSE] 2. dungeon.name: {}", dungeon.name);
-    println!("[VERBOSE] 3. selected_world: {}", multiverse_world_dungeon.selected_world);
+    if multiverse_world_dungeon.is_world_in_multiverse {
+      println!("[VERBOSE] W1. existing_worlds: {}", multiverse_world_dungeon.existing_worlds);
+      println!("[VERBOSE] W2. selected_world: {}", multiverse_world_dungeon.selected_world);  
+      println!("[VERBOSE] W3. world name: {}", multiverse_world_dungeon.name);
+    } else {
+      println!("[VERBOSE] W1. In Grevnyrch");
+    }
+
+    println!("[VERBOSE] 2. dungeon name: {}", dungeon.name);
   }
 }
